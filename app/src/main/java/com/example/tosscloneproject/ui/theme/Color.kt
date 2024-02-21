fe4dc14b4bdc8826718177a40e4555c26fda4515
chaeyoung <ocy5007@naver.com>
2024-02-21 15:18:59 +0900

    design: Color, Type 설정

--- app/src/main/java/com/example/tosscloneproject/ui/theme/Color.kt
+++ app/src/main/java/com/example/tosscloneproject/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+val TossBlue = Color(0xff2A7EF9) // 글씨 강조, 버튼 색깔
+val TextColor1 = Color(0xffADB3BA) // subtitle
+val TextColor2 = Color(0xff6C7684) // 부연설명
+val TextColor3 = Color(0xff323D4C) //
+val TextBlack = Color(0xff191F28) // 본문, 타이틀
--- app/src/main/java/com/example/tosscloneproject/ui/theme/Type.kt
+++ app/src/main/java/com/example/tosscloneproject/ui/theme/Type.kt
@@ -1,20 +1,49 @@
 package com.example.tosscloneproject.ui.theme
 
+//import androidx.compose.material3.Text
 import androidx.compose.material3.Typography
+//import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+//import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
+//    bodyLarge = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 16.sp,
+//        lineHeight = 24.sp,
+//        letterSpacing = 0.5.sp
+//    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 30.sp,
+        lineHeight = 35.sp,
+        color = TextBlack
+    ),
+    bodyMedium = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 18.sp,
+        lineHeight = 35.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        color = TextColor1
     )
+
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
