2f5fdf0ea1c4422204428487acb1ed643476a5fe
나희수 <gmltn328@gmail.com>
2024-02-21 21:40:00 +0900

    design: 내 자산 페이지 헤더 디자인 추가

---
+++ app/src/main/java/com/example/tosscloneproject/myAssetsPage/TopBar.kt
@@ -0,0 +1,56 @@
+package com.example.tosscloneproject.myAssetsPage
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tosscloneproject.R
+import com.example.tosscloneproject.ui.theme.TossCloneProjectTheme
+
+@Composable
+fun TopBar() {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .height(120.dp)
+        .background(color = Color.White)
+        ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 50.dp, start = 20.dp, end = 20.dp),
+            horizontalArrangement = Arrangement.SpaceBetween) {
+            Image(
+                painter = painterResource(id = R.drawable.arrow_left),
+                contentDescription = null,
+            )
+            Text(text = "편집")
+        }
+        Row(
+            modifier = Modifier
+                .padding(vertical = 10.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly) {
+            Text(text = "자산")
+            Text(text = "자산 굴리기")
+            Text(text = "대출 찾기")
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    TossCloneProjectTheme {
+        TopBar()
+    }
+}
