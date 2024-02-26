81314e1d916e042e14984eed5b33467f813951d5
chaeyoung <ocy5007@naver.com>
2024-02-26 11:10:30 +0900

    design: Color/Type 색깔 수정 및 타입 추가

--- app/src/main/java/com/example/tosscloneproject/ui/theme/Color.kt
+++ app/src/main/java/com/example/tosscloneproject/ui/theme/Color.kt
@@ -13,6 +13,6 @@ val Pink40 = Color(0xFF7D5260)
 
 val TossBlue = Color(0xff2A7EF9) // 글씨 강조, 버튼 색깔
 val TextColor1 = Color(0xffADB3BA) // subtitle
-val TextColor2 = Color(0xff6C7684) // 부연설명
-val TextColor3 = Color(0xff323D4C) //
+val TextColor2 = Color(0xff6B7684) // 부연설명
+val TextColor3 = Color(0xff323D4C) // Login Page
 val TextBlack = Color(0xff191F28) // 본문, 타이틀
