654597e8a33544be3b8593ccaef5f06e413b72e7
나희수 <gmltn328@gmail.com>
2024-02-26 21:57:00 +0900

    design: 네비게이션 라우트 경로 추가

--- app/src/main/java/com/example/tosscloneproject/NavigationGraph.kt
+++ app/src/main/java/com/example/tosscloneproject/NavigationGraph.kt
@@ -5,20 +5,28 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.tosscloneproject.mainPage.MainPageView
+import com.example.tosscloneproject.myAssetsPage.AssetList
 import com.example.tosscloneproject.myAssetsPage.MyAssetsPageView
+import com.example.tosscloneproject.selectBank.SelectBankView
 
 @Composable
 fun NavigationGraph() {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = "mainPage") {
-        composable("mainPage") {
+        composable("MainPage") {
             MainPageView(navController = navController) // 메인 페이지
         }
-        composable("checkPassword") {// 비밀번호 확인
+        composable("CheckPassword") {// 비밀번호 확인
             CheckPassword(navController = navController)
         }
-        composable("myAssetsPage") {
+        composable("MyAssetsPage") {
             MyAssetsPageView(navController = navController) // 내 자산 페이지
+        }
+        composable("AssetList") {
+            AssetList(navController = navController) // bottom sheet 내의 은행
+        }
+        composable("SelectBank") {
+            SelectBankView(navController = navController) // 은행 선택 페이지
         }
     }
 }
