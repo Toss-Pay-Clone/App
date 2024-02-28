55bbeaf98f8c15e349f08e4e73f772d6c4d4d73a
chaeyoung <ocy5007@naver.com>
2024-02-28 20:42:02 +0900

    feat: API 관련 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,6 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.6.0-alpha06")
     implementation ("androidx.compose.ui:ui:1.0.0")
     implementation ("androidx.compose.material:material:1.0.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
