980894be82545c9398a64018ccaaefc4ccf721cd
나희수 <gmltn328@gmail.com>
2024-02-29 15:32:29 +0900

    Design: 공용 버튼 다자인 추가

---
+++ app/src/main/java/com/example/tosscloneproject/publicComposable/CustomButton.kt
@@ -0,0 +1,41 @@
+package com.example.tosscloneproject.publicComposable
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tosscloneproject.ui.theme.TossBlue
+import com.example.tosscloneproject.ui.theme.TossCloneProjectTheme
+
+@Composable
+fun CustomButton(contents: String) {
+    Button(
+        onClick = {},
+        contentPadding = PaddingValues(8.dp),
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(25),
+        colors = ButtonDefaults.buttonColors(TossBlue),
+        ) {
+        Text(
+            text = contents,
+            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.SemiBold),
+            color = Color.White
+        )
+    }
+}
+@Composable
+@Preview(showBackground = true)
+fun CustomButtonPreview() {
+    TossCloneProjectTheme {
+        CustomButton(contents = "커스텀")
+    }
+}
