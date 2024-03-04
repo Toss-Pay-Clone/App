4eeeefa053fbefb9f2d3b215113f435d42e20d6b
나희수 <gmltn328@gmail.com>
2024-03-04 23:17:01 +0900

    Fix: 충돌 해결

--- app/src/main/java/com/example/tosscloneproject/MainActivity.kt
+++ app/src/main/java/com/example/tosscloneproject/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.tosscloneproject.Login.OnBoarding.NavigationGraph
-import com.example.tosscloneproject.mainPage.MainPageView
 import com.example.tosscloneproject.ui.theme.TossCloneProjectTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,11 +11,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
             TossCloneProjectTheme {
-
-                MainPageView()
-
+                NavigationGraph()
             }
         }
     }
