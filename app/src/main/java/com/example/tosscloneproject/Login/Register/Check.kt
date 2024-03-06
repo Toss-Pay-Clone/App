98e255034b78614699ca76cea41392b17e93b175
chaeyoung <ocy5007@naver.com>
2024-03-06 20:06:12 +0900

    feat: 회원가입 페이지 API 추가

--- app/src/main/java/com/example/tosscloneproject/Login/Register/Check.kt
+++ app/src/main/java/com/example/tosscloneproject/Login/Register/Check.kt
@@ -1,6 +1,8 @@
 package com.example.tosscloneproject.Login.Register
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -21,19 +23,36 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.tosscloneproject.Login.Compose.BirthNumberInput
+import com.example.tosscloneproject.Login.Compose.BirthNumberViewModel
 import com.example.tosscloneproject.Login.Compose.Button
-import com.example.tosscloneproject.Login.Compose.NumberInput
+import com.example.tosscloneproject.Login.Compose.GenderNumberInput
+import com.example.tosscloneproject.Login.Compose.GenderNumberViewModel
+import com.example.tosscloneproject.Login.Compose.PhoneNumberViewModel
 import com.example.tosscloneproject.Login.Compose.PhonenumberInput
+import com.example.tosscloneproject.Login.Compose.SignUpViewModel
+import com.example.tosscloneproject.Login.Compose.SignUpViewModelFactory
+import com.example.tosscloneproject.Login.Compose.TelecomViewModel
 import com.example.tosscloneproject.Login.Compose.UserInput
+import com.example.tosscloneproject.Login.Compose.UserNameViewModel
+import com.example.tosscloneproject.Login.OnBoarding.NAV_ROUTE
+import com.example.tosscloneproject.Login.OnBoarding.RouteAction
+import com.example.tosscloneproject.Login.signUpApiService
 import com.example.tosscloneproject.R
 import com.example.tosscloneproject.ui.theme.TextBlack
 import com.example.tosscloneproject.ui.theme.TextColor1
@@ -49,7 +68,7 @@ class Check : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    CheckPage()
+
                 }
             }
         }
@@ -60,13 +79,36 @@ class Check : ComponentActivity() {
 
 
 @Composable
-fun CheckPage() {
+fun CheckPage(routeAction: RouteAction, userNameViewModel: UserNameViewModel,
+              birthNumberViewModel: BirthNumberViewModel, genderNumberViewModel: GenderNumberViewModel,
+              phoneNumberViewModel: PhoneNumberViewModel, telecomViewModel: TelecomViewModel) {
     val typography = MaterialTheme.typography
     val image = painterResource(R.drawable.number_image)
     val arrow = painterResource(R.drawable.arrow_bottom)
+    val userName by userNameViewModel.userName.collectAsState()
+    val birthNumber by birthNumberViewModel.birthNumber.collectAsState()
+    val genderNumber by genderNumberViewModel.genderNumber.collectAsState()
+    val phoneNumber by phoneNumberViewModel.phoneNumber.collectAsState()
+    val telecom by telecomViewModel.telecom.collectAsState()
+
+    val factory = SignUpViewModelFactory(
+        signUpApiService,
+        userNameViewModel,
+        phoneNumberViewModel,
+        birthNumberViewModel,
+        genderNumberViewModel,
+        telecomViewModel
+    )
+    val viewModelStoreOwner = checkNotNull(LocalViewModelStoreOwner.current) {
+        "ViewModelStoreOwner not found"
+    }
+    val lifecycleOwner = checkNotNull(LocalLifecycleOwner.current) {
+        "LifecycleOwner not found"
+    }
+    val signUpViewModel = ViewModelProvider(viewModelStoreOwner, factory).get(SignUpViewModel::class.java)
 
     var showSheet by remember { mutableStateOf(false) }
-    var selectedTelecom by remember { mutableStateOf("통신사") }
+    var selectedTelecom by remember { mutableStateOf("$telecom") }
 
     if (showSheet) {
         BottomSheet(onDismiss = { showSheet = false }) { telecom ->
@@ -74,6 +116,7 @@ fun CheckPage() {
             showSheet = false
         }
     }
+
 
     Column (modifier = Modifier.fillMaxSize()){
 
@@ -89,16 +132,19 @@ fun CheckPage() {
                 style = typography.titleLarge
             )
 
-            PhonenumberInput(inputplaceholder = "휴대폰 번호")
+            PhonenumberInput(inputplaceholder = "$phoneNumber",
+                onDone = {}) { newPhone ->
+                phoneNumberViewModel.setPhoneNumber(newPhone)
+            }
 
             Column {
                 Text(text = "통신사", style = typography.labelSmall)
                 Row ( modifier = Modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween){
-                    Text(text = if (selectedTelecom == "통신사") "통신사" else selectedTelecom,
+                    Text(text = if (selectedTelecom == "$telecom") "$telecom" else selectedTelecom,
                         style = typography.labelMedium,
-                        color = if (selectedTelecom == "통신사") TextColor1 else TextBlack,
-                        modifier = Modifier.padding(top = 10.dp))
+                        color = TextBlack,
+                        modifier = Modifier.padding(top = 10.dp, start = 17.dp))
                     IconButton(onClick = { showSheet = true }) {
                         Image(painter = arrow, contentDescription = "arrow_bottom")
                     }
@@ -110,11 +156,15 @@ fun CheckPage() {
                 Text(text = "주민등록번호", style = typography.labelSmall)
                 Row ( verticalAlignment = Alignment.CenterVertically ){
                     Box(modifier = Modifier.width(134.dp)) {
-                        NumberInput(inputplaceholder = "")
+                        BirthNumberInput(inputplaceholder = "$birthNumber") { newBirth ->
+                            birthNumberViewModel.setBirthNumber(newBirth)
+                        }
                     }
                     Text(text = " - ", style = typography.titleLarge)
                     Box(modifier = Modifier.width(45.dp)) {
-                        NumberInput(inputplaceholder = "")
+                        GenderNumberInput(inputplaceholder = "$genderNumber") { newGender ->
+                            genderNumberViewModel.setGenderNumber(newGender)
+                        }
                     }
                     Spacer(modifier = Modifier.width(7.dp))
                     Image(painter = image, contentDescription = "Number")
@@ -123,22 +173,37 @@ fun CheckPage() {
 
             Column {
                 Text(text = "이름", style = typography.labelSmall)
-                UserInput(inputplaceholder = "")
+                UserInput (inputplaceholder = "$userName") { newName ->
+                    userNameViewModel.setUserName(newName)
+                }
             }
 
             Spacer(modifier = Modifier.height(100.dp))
-            Button(buttonText = "확인",
-                paddingValues = PaddingValues(start = 152.dp, end = 152.dp, top = 21.dp, bottom = 21.dp))
+            Button(
+                onClickAction = { signUpViewModel.signup() },
+                buttonText = "확인",
+                paddingValues = PaddingValues(start = 152.dp, end = 152.dp, top = 21.dp, bottom = 21.dp),
+                route = NAV_ROUTE.MainPage, routeAction = routeAction)
+
+
+            val signupResult by signUpViewModel.signupResult.collectAsState()
+            Log.d("CheckPage", "signupResult is $signupResult")
+            val context = LocalContext.current
+
+            LaunchedEffect(signupResult) {
+                signupResult.let { isSuccess ->
+                    Log.d("CheckPage", signupResult.toString())
+                    Log.d("CheckPage", "LaunchedEffect executed")
+                    if (isSuccess == true) {
+                        routeAction.navTo(NAV_ROUTE.MainPage)
+                    } else {
+                        Toast.makeText(context, "회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
         }
     }
 }
 
 
-@Preview(showBackground = true)
-@Composable
-fun CheckPreview() {
-    TossCloneProjectTheme {
-
-        CheckPage()
-    }
-}
--- app/src/main/java/com/example/tosscloneproject/Login/Register/Register.kt
+++ app/src/main/java/com/example/tosscloneproject/Login/Register/Register.kt
@@ -7,8 +7,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tosscloneproject.Login.Compose.BirthNumberViewModel
+import com.example.tosscloneproject.Login.Compose.GenderNumberViewModel
+import com.example.tosscloneproject.Login.Compose.PhoneNumberViewModel
+import com.example.tosscloneproject.Login.Compose.TelecomViewModel
+import com.example.tosscloneproject.Login.Compose.UserNameViewModel
+import com.example.tosscloneproject.Login.OnBoarding.NAV_ROUTE
+import com.example.tosscloneproject.Login.OnBoarding.RouteAction
+import com.example.tosscloneproject.mainPage.MainPageView
 import com.example.tosscloneproject.ui.theme.TossCloneProjectTheme
 
 class Login : ComponentActivity() {
@@ -21,7 +34,8 @@ class Login : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    NamePage()
+
+//                    RegisterNavigation()
                 }
             }
         }
@@ -30,13 +44,51 @@ class Login : ComponentActivity() {
 
 
 
+//@Composable
+//fun RegisterNavigation (startRoute: NAV_ROUTE = NAV_ROUTE.Name) {
+//
+//    // 네비게이션 컨트롤러
+//    val navController = rememberNavController()
+//
+//    // 네비게이션 라우트 액션
+//    val routeAction = remember(navController) { RouteAction(navController) }
+//
+//    val userNameViewModel : UserNameViewModel = viewModel()
+//    val birthNumberViewModel : BirthNumberViewModel = viewModel()
+//    val genderNumberViewModel : GenderNumberViewModel = viewModel()
+//    val telecomViewModel : TelecomViewModel = viewModel()
+//    val phoneNumberViewModel : PhoneNumberViewModel = viewModel()
+//
+//    // NavHost로 네비게이션 결정
+//    // 네비게이션 연결할 페이지 설정
+//    NavHost( navController, startRoute.routeName ) {
+//
+//        // 라우트 이름 = 화면의 키
+//            // 화면 = 값
+//        composable(NAV_ROUTE.Name.routeName){
+//            NamePage(routeAction=routeAction, userNameViewModel = userNameViewModel)}
+//        composable(NAV_ROUTE.ResidentNumber.routeName){
+//            ResidentNumberPage(routeAction=routeAction, userNameViewModel = userNameViewModel,
+//                birthNumberViewModel = birthNumberViewModel, genderNumberViewModel = genderNumberViewModel)}
+//        composable(NAV_ROUTE.Telecom.routeName){
+//            TelecomPage(routeAction=routeAction, userNameViewModel = userNameViewModel,
+//                birthNumberViewModel = birthNumberViewModel, genderNumberViewModel = genderNumberViewModel,
+//                telecomViewModel = telecomViewModel)}
+//        composable(NAV_ROUTE.Check.routeName){
+//            CheckPage(routeAction=routeAction, userNameViewModel = userNameViewModel,
+//                birthNumberViewModel = birthNumberViewModel, genderNumberViewModel = genderNumberViewModel,
+//                phoneNumberViewModel = phoneNumberViewModel, telecomViewModel = telecomViewModel)}
+//        composable(NAV_ROUTE.MainPage.routeName){ MainPageView() }
+//    }
+//}
+
 
 
 @Preview(showBackground = true)
 @Composable
 fun LoginPreview() {
     TossCloneProjectTheme {
+//        RegisterNavigation()
 
-        NamePage()
     }
 }
